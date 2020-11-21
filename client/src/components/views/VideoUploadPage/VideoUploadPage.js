3989f2a22ab5d51a7b9a73e0d7787e662adbc778
sjs <seo777111@gmail.com>
2020-11-21 19:07:36 +0900

    처음 커밋

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+function VideoUploadPage (){
+    return(
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+               
+            </div>
+
+            <Form onSubmit={onSubmit}>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                 </div>
+            </Form>
+    )
+}
+
+export default VideoUploadPage
