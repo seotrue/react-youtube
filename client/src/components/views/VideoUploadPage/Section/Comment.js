eb828a06e4778aaab19625f828d5db2f75403b21
sjs <seo777111@gmail.com>
2021-01-15 20:09:08 +0900

    sever delect

---
+++ client/src/components/views/VideoUploadPage/Section/Comment.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Comment() {'use  '
+    const 
+    return (
+        <div>
+            <form>
+                <textarea onChange></textarea>
+            </form>    
+        </div>
+    )
+}
+
+export default Comment
